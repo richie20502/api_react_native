58dcdeea8f9c1d5b147f95f7765075b8f9999618
richie20502 <richie.lugo.recillas.1990@gmail.com>
2024-06-19 20:52:37 -0600

    de agrego url configurtacion swagger

--- docs/swagger.js
+++ docs/swagger.js
@@ -11,7 +11,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:3000"
+                url: "https://api-react-native-ys7b.onrender.com"
             }
         ]
     },
